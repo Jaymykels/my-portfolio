428396595041585d4114f51bc256696903db4c35
Jaymykels69 <Jaymykels69@gmail.com>
2019-04-18 08:54:09 +0100

    installed templete

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,166 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <!-- BACKGROUND GRID START -->
+    <div class="background-grid height-100 position-fixed">
+      <span class="column height-100"></span>
+      <span class="column height-100"></span>
+      <span class="column height-100"></span>
+      <span class="column height-100 hidden-xs"></span>
+      <span class="column height-100 hidden-xs hidden-sm"></span>
+      <span class="column height-100 hidden-xs hidden-sm"></span>
     </div>
+    <!-- //BACKGROUND GRID END -->
+
+    <!-- HEADER START -->
+    <header class="header">
+      <div class="logo btn-shadow text-center">
+        <div class="display-table height-100 width-100">
+          <div class="display-table-cell vertical-align-middle">
+            <span
+              class="display-inline-block font-weight-700 title-large position-relative"
+              >N</span
+            >
+          </div>
+          <!-- //.display-table-cell -->
+        </div>
+        <!-- //.display-table -->
+      </div>
+      <!-- //.logo -->
+
+      <div class="nav-trigger nav-trigger-open">
+        <div class="nav-toggle">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <!-- //.nav-toggle -->
+      </div>
+      <!-- //.nav-trigger -->
+    </header>
+    <!-- //HEADER END -->
+
+    <!-- NAVIGATION START -->
+    <div id="navigation" class="navbar navbar-display-block" role="navigation">
+      <div class="container-fluid display-table height-100 width-100">
+        <div class="display-table-cell vertical-align-middle width-100">
+          <div class="nav-trigger nav-trigger-close">
+            <span></span>
+            <span></span>
+          </div>
+          <!-- //.nav-trigger -->
+
+          <ul class="nav navbar-nav text-center text-small text-uppercase">
+            <li class="page-scroll"><a href="#home">Home</a></li>
+            <li class="page-scroll"><a href="#profile">Profile</a></li>
+            <li class="page-scroll"><a href="#work-experiences">Resume</a></li>
+            <li class="page-scroll"><a href="#skills-bar-chart">Skills</a></li>
+            <li class="page-scroll"><a href="#contact">Contact</a></li>
+          </ul>
+          <!-- //.navbar-nav -->
+
+          <div class="nav-social hidden-md hidden-lg text-center">
+            <ul
+              class="list-unstyled list-inline no-letter-spacing title-medium"
+            >
+              <li>
+                <a href="#" target="_blank"
+                  ><i class="ion-social-facebook"></i
+                ></a>
+              </li>
+              <li>
+                <a href="#" target="_blank"
+                  ><i class="ion-social-twitter"></i
+                ></a>
+              </li>
+              <li>
+                <a href="#" target="_blank"
+                  ><i class="ion-social-dribbble"></i
+                ></a>
+              </li>
+              <li>
+                <a href="#" target="_blank"
+                  ><i class="ion-social-github"></i
+                ></a>
+              </li>
+              <li>
+                <a href="#" target="_blank"
+                  ><i class="ion-social-linkedin"></i
+                ></a>
+              </li>
+            </ul>
+
+            <span class="display-inline-block text-large">hello@noya.com</span>
+          </div>
+          <!-- //.nav-social -->
+        </div>
+        <!-- //.display-table-cell -->
+      </div>
+      <!-- //.container-fluid -->
+    </div>
+    <!-- //NAVIGATION END -->
+
     <router-view />
+
+    <!-- FOOTER START -->
+    <footer class="footer position-relative">
+      <div
+        class="page-scroll background-color display-block position-absolute text-center"
+      >
+        <a
+          href="#page-top"
+          class="btn-shadow display-block text-white title-medium"
+        >
+          <i class="ion-chevron-up"></i>
+        </a>
+      </div>
+      <!-- //.page-scroll -->
+
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <ul
+              class="list-unstyled list-inline margin-2 no-margin-top no-margin-rl no-letter-spacing title-medium"
+            >
+              <li>
+                <a href="#" target="_blank"
+                  ><i class="ion-social-facebook"></i
+                ></a>
+              </li>
+              <li>
+                <a href="#" target="_blank"
+                  ><i class="ion-social-twitter"></i
+                ></a>
+              </li>
+              <li>
+                <a href="#" target="_blank"
+                  ><i class="ion-social-dribbble"></i
+                ></a>
+              </li>
+              <li>
+                <a href="#" target="_blank"
+                  ><i class="ion-social-github"></i
+                ></a>
+              </li>
+              <li>
+                <a href="#" target="_blank"
+                  ><i class="ion-social-linkedin"></i
+                ></a>
+              </li>
+            </ul>
+
+            <p class="font-family-alt text-medium">
+              <strong>&copy; 2016</strong> Noya &mdash; One Page Personal Theme.
+              <strong>Developed</strong> by Andy Noya.
+            </p>
+            <p class="font-family-alt text-medium">
+              <strong>Design</strong> by Noya Studio.
+            </p>
+          </div>
+          <!-- //.col-md-12 -->
+        </div>
+        <!-- //.row -->
+      </div>
+      <!-- //.container -->
+    </footer>
   </div>
 </template>
-
-<style>
-  @tailwind preflight;
-  @tailwind components;
-  @tailwind utilities;
-</style>
