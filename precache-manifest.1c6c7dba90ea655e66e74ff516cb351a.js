21bad39daa25414e17dcd67a2a2d1c702cba7d9a
Michael Ezeokoye <jaymykels69@gmail.com>
2022-10-08 22:52:31 +0100

    deploy

---
+++ precache-manifest.1c6c7dba90ea655e66e74ff516cb351a.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e1759630cdf41d6621a",
+    "url": "/my-portfolio/js/app.52cb5feb.js"
+  },
+  {
+    "revision": "19fcc8d5e36127eb8119",
+    "url": "/my-portfolio/js/chunk-vendors.1022c17d.js"
+  },
+  {
+    "revision": "9931b2c419f08449ffce9b469f67390d",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "20f3d4b6c303d834b09127eaff66b5cc",
+    "url": "/my-portfolio/css/styles.min.css"
+  },
+  {
+    "revision": "54f662b4d95455fc738a4f0648013515",
+    "url": "/my-portfolio/img/avatar.jpeg"
+  },
+  {
+    "revision": "4ca7d4c4e6a05dbfaf7f97b5f4017dbb",
+    "url": "/my-portfolio/favicon-32x32.png"
+  },
+  {
+    "revision": "3af34c84f53dfbac36e1f095e2395fbe",
+    "url": "/my-portfolio/img/projects/dsssapa.png"
+  },
+  {
+    "revision": "a1de2afe67b014ecb30e3bb2daef21f4",
+    "url": "/my-portfolio/js/main.js"
+  },
+  {
+    "revision": "b4f0e6aa48417c9cf5549e48ffe24fc2",
+    "url": "/my-portfolio/img/projects/stears.png"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/my-portfolio/plugins/bootstrap/fonts/glyphicons-halflings-regular.eot"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/my-portfolio/plugins/bootstrap/fonts/glyphicons-halflings-regular.html"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/my-portfolio/plugins/bootstrap/fonts/glyphicons-halflings-regular.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/my-portfolio/plugins/bootstrap/fonts/glyphicons-halflings-regular.svg"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/my-portfolio/plugins/bootstrap/fonts/glyphicons-halflings-regulard41d.eot"
+  },
+  {
+    "revision": "188515bbd442366a90a1cce99af4e48f",
+    "url": "/my-portfolio/plugins/bootstrap/css/bootstrap.min.css"
+  },
+  {
+    "revision": "69bf80f5a583263d6477ab50d20e8198",
+    "url": "/my-portfolio/plugins/bootstrap/js/bootstrap.min.js"
+  },
+  {
+    "revision": "cb13ce6e9b41da9553d9d9266dcb6f62",
+    "url": "/my-portfolio/plugins/easing/jquery.easing.min.js"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/my-portfolio/plugins/bootstrap/fonts/glyphicons-halflings-regular.woff"
+  },
+  {
+    "revision": "70f5c971d00f360accd97e1c4b60a99d",
+    "url": "/my-portfolio/plugins/ionicons/css/ionicons.min.css"
+  },
+  {
+    "revision": "1b2123c1231a4bf096e8d9691fe30c13",
+    "url": "/my-portfolio/plugins/debouncer/debouncer.js"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/my-portfolio/plugins/ionicons/fonts/ionicons28b5.eot"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/my-portfolio/plugins/ionicons/fonts/ionicons28b5.ttf"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/my-portfolio/plugins/ionicons/fonts/ionicons28b5.woff"
+  },
+  {
+    "revision": "656eb86aa0f49eeddc229b3fc7009689",
+    "url": "/my-portfolio/plugins/jpreloader/js/jpreloader.min.js"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/my-portfolio/plugins/ionicons/fonts/ionicons28b5.svg"
+  },
+  {
+    "revision": "2f6b11a7e914718e0290410e85366fe9",
+    "url": "/my-portfolio/plugins/jquery/jquery.min.js"
+  },
+  {
+    "revision": "ba6cf724c8bb1cf5b084e79ff230626e",
+    "url": "/my-portfolio/plugins/magnific-popup/jquery.magnific-popup.min.js"
+  },
+  {
+    "revision": "6a51162d9d8e2609e2d14193918620cd",
+    "url": "/my-portfolio/plugins/magnific-popup/magnific-popup.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/my-portfolio/robots.txt"
+  },
+  {
+    "revision": "91aada6caa0c351f0830106fadfc0272",
+    "url": "/my-portfolio/plugins/jpreloader/css/jpreloader.css"
+  },
+  {
+    "revision": "18da8a12cc5801bc9e46ec149cb4ab5b",
+    "url": "/my-portfolio/img/projects/foodlocker.png"
+  },
+  {
+    "revision": "d81c495324867a61a9e26c7a66fd3dd0",
+    "url": "/my-portfolio/img/projects/folio.png"
+  }
+];
